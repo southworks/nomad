56a774c4d6e9d1919733cfb07d7675ef745d721b
Jose Ignacio Lorenzo <joseignaciolorenzo85@gmail.com>
2023-03-07 15:00:33 -0300

    [CONSUL-683] Update tests

--- nomad/structs/services_test.go
+++ nomad/structs/services_test.go
@@ -871,6 +871,12 @@ var (
 					Name:  "service2",
 					Hosts: []string{"10.0.0.2", "10.0.0.2:3000"},
 				}},
+				TLS: &ConsulGatewayTLSConfig{
+					SDS: &ConsulGatewayTLSSDSConfig{
+						ClusterName:  "foo",
+						CertResource: "bar",
+					},
+				},
 			}, {
 				Port:     3001,
 				Protocol: "tcp",
@@ -1049,6 +1055,12 @@ func TestConsulGateway_Equal_ingress(t *testing.T) {
 		try(t, func(g *cg) { g.Ingress.Listeners[0].Port = 7777 })
 	})
 
+	t.Run("mod ingress listeners tls sds clustername and certresource", func(t *testing.T) {
+		try(t, func(g *cg) {
+			g.Ingress.Listeners[0].TLS.SDS = &ConsulGatewayTLSSDSConfig{ClusterName: "baz", CertResource: "qux"}
+		})
+	})
+
 	t.Run("mod ingress listeners protocol", func(t *testing.T) {
 		try(t, func(g *cg) { g.Ingress.Listeners[0].Protocol = "tcp" })
 	})
@@ -1164,6 +1176,12 @@ func TestConsulGateway_ingressListenersEqual(t *testing.T) {
 			Name:  "service1",
 			Hosts: []string{"host1", "host2"},
 		}},
+		TLS: &ConsulGatewayTLSConfig{
+			SDS: &ConsulGatewayTLSSDSConfig{
+				ClusterName:  "foo",
+				CertResource: "bar",
+			},
+		},
 	}, {
 		Port:     2001,
 		Protocol: "tcp",
